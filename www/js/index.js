f1ea1d96c740077816c5f52a615b2717ac873210
pingus84 <32274474+pingus84@users.noreply.github.com>
2026-04-08 13:00:52 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -23,6 +23,69 @@ function logHasPermissionOnStart() {
 
 
 
+function notifauto() {
+/* Your code. (Make sure to not alert(status) until splashscreen
+ * has been hidden with navigator.splashscreen.hide() 
+ * else alert might not show and your app seems to stall.)
+ */
+let permissionPlugin = window.cordova.notifications_permission;
+let rationaleTitle = "Notification Permission";
+let rationaleMsg = "You really need to give permission!";
+let rationaleOkButton = "OK";
+let rationaleCancelButton = "Not now";
+let rationaleTheme = permissionPlugin.themes.Theme_DeviceDefault_Dialog_Alert;
+let lastResortTitle = "Notification Permission!";
+let lastResortMsg = "You really need to give permission! Now the only way left is through system settings.";
+let lastResortOkButton = "Settings";
+let lastResortCancelButton = "No thanks";
+let lastResortTheme = permissionPlugin.themes.Theme_DeviceDefault_Dialog_Alert;
+permissionPlugin.maybeAskPermission(
+    function(status) {
+        /* Permission is either granted, denied, or not needed. */
+        switch(status){
+            case permissionPlugin.GRANTED_NEWLY_WITHOUT_RATIONALE:
+            case permissionPlugin.GRANTED_NEWLY_AFTER_RATIONALE:
+            case permissionPlugin.GRANTED_NEWLY_AFTER_SETTINGS:
+            case permissionPlugin.GRANTED_ALREADY:
+            case permissionPlugin.NOT_NEEDED:
+                /* Notification shows the same as it did before Android 13 (API Level 33). */
+                break;
+            case permissionPlugin.DENIED_NOT_PERMANENTLY_NEWLY:
+            case permissionPlugin.DENIED_PERMANENTLY_NEWLY:
+            case permissionPlugin.DENIED_NOT_PERMANENTLY_ALREADY:
+            case permissionPlugin.DENIED_PERMANENTLY_ALREADY:
+            case permissionPlugin.DENIED_PERMANENTLY_ALREADY_AFTER_SETTINGS
+            case permissionPlugin.DENIED_THROUGH_RATIONALE_DIALOG:
+            case permissionPlugin.DENIED_THROUGH_LAST_RESORT_DIALOG:
+            case permissionPlugin.NOT_ANDROID:
+                /* The notification does not show. */
+                break;    
+            case permissionPlugin.ERROR:
+                /* See console for error message */
+                break;
+        }
+    },
+    {
+        show: true,
+        title:rationaleTitle,
+        msg: rationaleMsg,
+        okButton: rationaleOkButton,
+        cancelButton: rationaleCancelButton,
+        theme: rationaleTheme
+    },
+    {
+        show: true,
+        title:lastResortTitle,
+        msg: lastResortMsg,
+        okButton: lastResortOkButton,
+        cancelButton: lastResortCancelButton,
+        theme: lastResortTheme
+    }
+);
+/* END OF Your code */
+}		
+
+
 function trySomeTimes(asyncFunc, onSuccess, onFailure, customTries) {
   var tries = typeof customTries === "undefined" ? 100 : customTries;
   var interval = setTimeout(function () {
@@ -303,65 +366,7 @@ var app = {
     onDeviceReady: function() {
 
 
-/* Your code. (Make sure to not alert(status) until splashscreen
- * has been hidden with navigator.splashscreen.hide() 
- * else alert might not show and your app seems to stall.)
- */
-let permissionPlugin = window.cordova.notifications_permission;
-let rationaleTitle = "Notification Permission";
-let rationaleMsg = "You really need to give permission!";
-let rationaleOkButton = "OK";
-let rationaleCancelButton = "Not now";
-let rationaleTheme = permissionPlugin.themes.Theme_DeviceDefault_Dialog_Alert;
-let lastResortTitle = "Notification Permission!";
-let lastResortMsg = "You really need to give permission! Now the only way left is through system settings.";
-let lastResortOkButton = "Settings";
-let lastResortCancelButton = "No thanks";
-let lastResortTheme = permissionPlugin.themes.Theme_DeviceDefault_Dialog_Alert;
-permissionPlugin.maybeAskPermission(
-    function(status) {
-        /* Permission is either granted, denied, or not needed. */
-        switch(status){
-            case permissionPlugin.GRANTED_NEWLY_WITHOUT_RATIONALE:
-            case permissionPlugin.GRANTED_NEWLY_AFTER_RATIONALE:
-            case permissionPlugin.GRANTED_NEWLY_AFTER_SETTINGS:
-            case permissionPlugin.GRANTED_ALREADY:
-            case permissionPlugin.NOT_NEEDED:
-                /* Notification shows the same as it did before Android 13 (API Level 33). */
-                break;
-            case permissionPlugin.DENIED_NOT_PERMANENTLY_NEWLY:
-            case permissionPlugin.DENIED_PERMANENTLY_NEWLY:
-            case permissionPlugin.DENIED_NOT_PERMANENTLY_ALREADY:
-            case permissionPlugin.DENIED_PERMANENTLY_ALREADY:
-            case permissionPlugin.DENIED_PERMANENTLY_ALREADY_AFTER_SETTINGS
-            case permissionPlugin.DENIED_THROUGH_RATIONALE_DIALOG:
-            case permissionPlugin.DENIED_THROUGH_LAST_RESORT_DIALOG:
-            case permissionPlugin.NOT_ANDROID:
-                /* The notification does not show. */
-                break;    
-            case permissionPlugin.ERROR:
-                /* See console for error message */
-                break;
-        }
-    },
-    {
-        show: true,
-        title:rationaleTitle,
-        msg: rationaleMsg,
-        okButton: rationaleOkButton,
-        cancelButton: rationaleCancelButton,
-        theme: rationaleTheme
-    },
-    {
-        show: true,
-        title:lastResortTitle,
-        msg: lastResortMsg,
-        okButton: lastResortOkButton,
-        cancelButton: lastResortCancelButton,
-        theme: lastResortTheme
-    }
-
-
+	notifauto();
 		
 	ons.disableAutoStatusBarFill() ;
 	
